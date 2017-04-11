3efca79d9aa4b70911175a43e2a72fa7a4590f32
Nirmal <ksnirm@gmail.com>
2017-04-11 12:54:57 +0530

    home api fetch

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,1 +1,30 @@
-//var app = angular.module('video-rental',[]);
+var app = angular.module('video-rental', ['ngResource', 'ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'HomeCtrl'
+        })
+        .when('/blue', {
+
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+app.controller('HomeCtrl', ['$scope', '$resource',
+    function($scope, $resource) {
+        var videos = $resource('/apis/videos');
+        videos.query(function(video) {
+            console.log(video);
+            $scope.videos = video;
+        })
+    }
+]);
+
+//
+// app.controller('HomeCtrl', function($scope, $resouce) {
+//
+// });
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('video', { title: 'Vidzy' ,user_name:'Nirmal Kumar'});
+    res.render('index', {
+        title: 'Vidzy',
+        user_name: 'Nirmal Kumar'
+    });
 });
 
 module.exports = router;
